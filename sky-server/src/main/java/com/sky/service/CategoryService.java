4cb6ef51d0a226bc69292b3ce0255d91598984d4
zelong <dlutlouis@icloud.com>
2025-11-24 02:23:09 -0600

    using AOP to autofill the common fields

--- sky-server/src/main/java/com/sky/service/CategoryService.java
+++ sky-server/src/main/java/com/sky/service/CategoryService.java
@@ -1,8 +1,10 @@
 package com.sky.service;
 
+import com.sky.annotation.AutoFill;
 import com.sky.dto.CategoryDTO;
 import com.sky.dto.CategoryPageQueryDTO;
 import com.sky.entity.Category;
+import com.sky.enumeration.OperationType;
 import com.sky.result.PageResult;
 import java.util.List;
 
