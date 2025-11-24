ec9fd1660e7470a310113ccb3a4fa7c2180e8a62
zelong <dlutlouis@icloud.com>
2025-11-24 03:05:42 -0600

    WIP:insert dish

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,5 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -14,4 +18,9 @@ public interface DishMapper {
     @Select("select count(id) from dish where category_id = #{categoryId}")
     Integer countByCategoryId(Long categoryId);
 
+    @AutoFill(OperationType.INSERT)
+    @Insert("INSERT INTO dish (name, category_id, price, image, description, create_time, update_time, create_user, update_user,status)"+
+    "VALUES "+
+            "(#{name},#{categoryId},#{price},#{image},#{description},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status})")
+    void insert(Dish dish);
 }
