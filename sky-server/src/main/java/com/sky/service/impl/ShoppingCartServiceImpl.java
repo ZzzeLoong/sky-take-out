0326b0d78cf38c385cdd3f3527da05ae54d1134a
zelong <dlutlouis@icloud.com>
2025-11-27 19:14:31 -0600

    implement shopping cart and address management

---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,75 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ShoppingCartServiceImpl implements ShoppingCartService{
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+
+    @Override
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO,shoppingCart);
+
+        shoppingCart.setUserId(BaseContext.getCurrentId());
+        List<ShoppingCart> shoppingCartList = shoppingCartMapper.list(shoppingCart);
+        if(shoppingCartList!=null&& shoppingCartList.size()==1){
+            shoppingCart = shoppingCartList.get(0);
+            shoppingCart.setNumber(shoppingCart.getNumber()+1);
+            shoppingCartMapper.updateNumberById(shoppingCart);
+        }else{
+            Long dishId = shoppingCartDTO.getDishId();
+            if(dishId!=null){
+                Dish dish = dishMapper.getById(dishId);
+                shoppingCart.setAmount(dish.getPrice());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setName(dish.getName());
+
+            }else{
+                Setmeal setmeal = setmealMapper.getById(shoppingCart.getSetmealId());
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+        }
+
+    }
+
+    @Override
+    public List<ShoppingCart> showShoppingCart() {
+        Long UserId = BaseContext.getCurrentId();
+        return shoppingCartMapper.list(ShoppingCart.builder().userId(UserId).build());
+    }
+
+    @Override
+    public void clean() {
+        Long UserId = BaseContext.getCurrentId();
+        shoppingCartMapper.cleanById(UserId);
+    }
+}
